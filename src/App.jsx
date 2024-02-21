4acb2d79a7d528a9b56589997edb951f6472bd7b
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-21 10:45:02 +0400

    Project Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import { HomePage } from './components/Home.page.jsx';
+import { RQSuperHeroesPage } from './components/RQSuperHeroes.page.jsx';
+import { SuperHeroesPage } from './components/SuperHeroes.page.jsx';
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
