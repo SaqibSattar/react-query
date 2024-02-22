e15568217cc21c750beb1461132b726b0aaf46de
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-22 11:19:46 +0400

    Success and Error Callbacks

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,14 +1,31 @@
 import { useQuery } from 'react-query'
 import axios from 'axios'
+import { useState } from 'react'
 
 const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes')
 }
 
 export const RQSuperHeroesPage = () => {
+  const[refetchIntervalTime, setRefetchIntervalTime] = useState(3000)
+
+  const onSuccess = data => {
+    if(data?.data.length === 3) {
+      setRefetchIntervalTime(0)
+    }
+    console.log({ data })
+  }
+
+  const onError = error => {
+    setRefetchIntervalTime(0)
+    console.log({ error })
+  }
+
   const { isLoading, data , isError, error, isFetching, refetch } = useQuery('super-heroes', fetchSuperHeroes,
   {
-    enabled: false,
+    refetchInterval: refetchIntervalTime,
+    onSuccess,
+    onError
   })
   console.log({isLoading, isFetching })
   // const { isLoading, data } = useQuery('super-heroes', () => {
@@ -26,7 +43,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      <button onClick={refetch}>Fetch heroes</button>
+      <button onClick={() => refetch()}>Fetch heroes</button>
       {data?.data.map(hero => {
         return <div key={hero.id}>{hero.name}</div>
       })}
